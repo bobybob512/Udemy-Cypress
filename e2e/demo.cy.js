50edc271c5c2e732396a20795d5b382bf90bbe50
Bob <>
2024-05-27 13:31:29 +0300

    Test hooks and tags(beforeEach, AfterEach and Only)

--- e2e/demo.cy.js
+++ e2e/demo.cy.js
@@ -1,24 +1,28 @@
 /// <reference types="Cypress" />
 
 describe('Cypress E2E Testing Demo', () => {
-  it('Assert Url', () => {
+  beforeEach(() =>{
     cy.visit('https://example.com')
+  })
+
+  afterEach(() =>{
+    cy.log('This  will log after each of my test')
+  })
+
+  it('Assert Url', () => {
     cy.url().should('contain', 'example.com')
   })
 
   it('Assert title', () =>{
-    cy.visit('https://example.com')
     cy.title().should('contain', 'Example Domain')
   })
 
-  it('Assert Elements', () =>{
-    cy.visit('https://example.com')
+  it.only('Assert Elements', () =>{
     cy.get('h1').should('be.visible')
     cy.get('p').should('be.visible')
   })
 
   it('Reloads and Logs', () =>{
-    cy.visit('https://example.com')
     cy.log('Before Reload')
     cy.reload()
     cy.log('After Reload')
