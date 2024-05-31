b107066e96bbdb677aa7c9755fb5565b21efbddc
Bob <>
2024-05-31 14:13:49 +0300

    closin the modal window

--- e2e/modals.cy.js
+++ e2e/modals.cy.js
@@ -5,5 +5,7 @@ describe('Cypress modals', () => {
     cy.visit('https://practice-automation.com/modals/')
     cy.get('#simpleModal').should('be.visible').click()
     cy.get('p').should('be.visible').should('contain', 'Hi, I’m a simple modal.')
+    cy.get('button.pum-close.popmake-close[aria-label="Close"]').last().click();
+
   })
 })
