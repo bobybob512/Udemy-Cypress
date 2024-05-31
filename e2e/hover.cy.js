b71fa73e76401d632f026634f775651eee5495e9
Bob <>
2024-05-31 13:19:32 +0300

    viewport, emultaion

--- e2e/hover.cy.js
+++ e2e/hover.cy.js
@@ -2,8 +2,9 @@
 
 describe('Cypress hover', () => {
   it('Hover workaround', () => {
+    cy.viewport(800, 600)
     cy.visit('https://practice-automation.com/hover/')
     cy.get('#mouse_over').trigger('mouseover')
-    cy.get('#mouse_over').should('contain', 'ou did it!')
+    cy.get('#mouse_over').should('contain', 'You did it!')
   })
 })
