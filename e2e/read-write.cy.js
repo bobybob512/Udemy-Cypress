0bb4e4119a9f92cfc9ecc885f6b50c0b5601af25
Bob <>
2024-05-31 16:31:01 +0300

    read from file and assert

--- e2e/read-write.cy.js
+++ e2e/read-write.cy.js
@@ -1,5 +1,10 @@
 describe('Cypress files', () => {
   it('Read/Write into Json file', () => {
     cy.writeFile('test-data/data.json', {name: 'Bob', email: 'bob@mail.com'})
+
+    cy.readFile('test-data/data.json').then((Whatever) => {
+      expect(Whatever.name).to.equal('Bob')
+      expect(Whatever.email).to.equal('bob@mail.com')
+    })
   })
 })
