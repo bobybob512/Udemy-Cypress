3f23a9fa2ec60b5f994b15a83e72c81f7e3afdcc
Bob <>
2024-06-03 15:08:25 +0300

    upload JSON example file

--- e2e/file-upload.cy.js
+++ e2e/file-upload.cy.js
@@ -1,5 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+/// <reference types="Cypress" />
+
+describe('Cypress file upload', () => {
+  it('Should upload JSON example file', () => {
+    cy.visit('https://the-internet.herokuapp.com/upload')
+    cy.get('#file-upload').selectFile('cypress/fixtures/example.json')
+    cy.get('#file-submit').click()
+
+    cy.get('h3').should('be.visible')
   })
 })
