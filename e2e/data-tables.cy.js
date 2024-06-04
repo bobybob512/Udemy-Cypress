5fb233c8bfe1a4798432eca9d64faf6afd94cbe7
Bob <>
2024-06-04 15:54:01 +0300

    data-tables

---
+++ e2e/data-tables.cy.js
@@ -0,0 +1,11 @@
+/// <reference types="Cypress" />
+
+describe('Data tables', () => {
+  it('Validate items inside data table', () => {
+    cy.visit('https://practice-automation.com/tables/')
+    cy.get('.wp-block-table').within(() => {
+      cy.get('td').eq(0).should('contain', 'Item')
+      cy.get('td').eq(1).should('contain', 'Price')
+    })
+  })
+})
